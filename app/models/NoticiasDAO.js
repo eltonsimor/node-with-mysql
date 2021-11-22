4b419a0e212807fca26ef61fb82e18d1b2b5a5a5
Elton Moraes <eltonsimor@gmail.com>
2021-11-21 21:35:07 -0300

    Refactory do codigo

---
+++ app/models/NoticiasDAO.js
@@ -0,0 +1,24 @@
+class NoticiasDAO
+ {
+
+    constructor(connection) {
+        this._connection = connection;
+    }
+
+    getNoticias(callback) {
+        this._connection.query('select * from noticias', callback);
+    }
+
+    getNoticia(callback) {
+        this._connection.query('select * from noticias where id_noticia = 1', callback);
+    }
+    
+    salvarNoticia(noticia, callback) {
+        console.log(noticia);
+        this._connection.query('insert into noticias set ?', noticia, callback);
+    }
+}
+
+module.exports = () => {
+    return NoticiasDAO;
+}
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,19 +1,36 @@
-module.exports = (app) => {
+ module.exports = (app) => {
     app.get('/formulario_inclusao_noticia', (req, res) => {
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {validacao: {}, noticia: {}});
     });
 
     app.post('/noticias/salvar', (req, res) => {
         var noticia = req.body;
 
-        var connection = app.config.dbConnection();
-        var noticiasModel = app.app.models.noticiasModel;
+        req.assert('titulo','Título é obrigatório').notEmpty();
+        req.assert('resumo','Resumo é obrigatório').notEmpty();
+        req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor','Autor é obrigatório').notEmpty();
+        req.assert('autor','Autor deve conter no máximo 30 caracteres').len(1, 30);
+        req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia','Notícia é obrigatório').notEmpty();
 
-        noticiasModel.salvarNoticia(connection, noticia, (error, result) => {
+        var errors = req.validationErrors();
+
+        console.log(errors);
+
+        if(errors) {
+            res.render('admin/form_add_noticia', {validacao: errors, noticia: noticia});
+            return;
+        }
+
+        var connection = app.config.dbConnection();
+        var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+        noticiasModel.salvarNoticia(noticia, (error, result) => {
             if(error){
                 console.log(error);
             }
-            res.redirect('/noticias')
+            res.redirect('/noticias');
         });
     });
 }
